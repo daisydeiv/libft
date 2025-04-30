3e5c47471584d2617af8ee482565e3e879240500
daisydev42 <desiree.lebriero@gmail.com>
2025-04-30 13:35:55 +0200

    added functions

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ OBJDIR := obj
 SRCS = ft_atoi.c ft_bzero.c ft_calloc.c ft_isalnum.c ft_isalpha.c \
 	ft_isascii.c ft_isdigit.c ft_isprint.c ft_memchr.c ft_memcmp.c \
 	ft_memcpy.c ft_memmove.c ft_memset.c ft_strchr.c ft_strrchr.c \
+	ft_strcat.c ft_strcpy.c \
 	ft_strdup.c ft_strlcat.c ft_strlcpy.c ft_strlen.c ft_strncmp.c \
 	ft_strnstr.c ft_tolower.c ft_toupper.c ft_substr.c ft_strjoin.c \
 	ft_strtrim.c ft_split.c ft_itoa.c ft_strmapi.c ft_striteri.c \
@@ -15,7 +16,7 @@ SRCS = ft_atoi.c ft_bzero.c ft_calloc.c ft_isalnum.c ft_isalpha.c \
 	ft_lstnew_bonus.c ft_lstadd_front_bonus.c ft_lstsize_bonus.c \
 	ft_lstlast_bonus.c ft_lstadd_back_bonus.c ft_lstdelone_bonus.c \
 	ft_lstclear_bonus.c ft_lstiter_bonus.c ft_lstmap_bonus.c \
-	get_next_line_bonus.c \
+	get_next_line.c \
 	ft_printf.c print_char.c print_string.c print_num.c print_unsigned.c \
 	print_lowhex.c print_uphex.c print_pointer.c \
 	ft_utoa.c hexa_convert.c \
--- get_next_line.c
+++ get_next_line.c
@@ -1,97 +1,119 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mle-brie <mle-brie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mle-brie <mle-brie@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 14:40:42 by mle-brie          #+#    #+#             */
-/*   Updated: 2024/12/06 15:16:45 by mle-brie         ###   ########.fr       */
+/*   Updated: 2025/04/29 09:43:21 by mle-brie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*read_and_store(int fd, char *stock, char *buffer)
+char	*ft_read_and_update_stock(int fd, char *stock, char *buffer)
 {
-	ssize_t		bytes_read;
-	char		*tmp;
+	int		bytes_read;
+	char	*temp;
 
+	temp = NULL;
 	bytes_read = 1;
 	while (bytes_read > 0)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read == -1)
+		if (bytes_read <= 0)
 			break ;
 		buffer[bytes_read] = '\0';
-		tmp = ft_strjoin(stock, buffer);
-		if (!tmp)
+		temp = ft_strjoin(stock, buffer);
+		if (!temp)
 			return (free(stock), NULL);
 		free(stock);
-		stock = tmp;
+		stock = temp;
 		if (ft_strchr(stock, '\n'))
 			break ;
 	}
 	if (bytes_read < 0)
 	{
-		free (stock);
+		free(stock);
 		stock = NULL;
 	}
 	return (stock);
 }
 
-char	*extract_line(char **stock)
+char	*ft_extract_line_from_stock(char **stock)
 {
-	char	*newline_pos;
-	size_t	line_size;
 	char	*line;
+	char	*newline_ptr;
+	char	*temp;
+	size_t	line_length;
 
-	newline_pos = ft_strchr(*stock, '\n');
-	if (newline_pos)
+	newline_ptr = ft_strchr(*stock, '\n');
+	if (!newline_ptr)
+		return (NULL);
+	line_length = newline_ptr - *stock + 1;
+	line = (char *)malloc(sizeof(char) * (line_length + 1));
+	if (!line)
+		return (NULL);
+	ft_strlcpy(line, *stock, line_length + 1);
+	temp = ft_strdup(newline_ptr + 1);
+	if (!temp)
+		return (free(line), NULL);
+	free(*stock);
+	*stock = temp;
+	if ((*stock)[0] == '\0')
 	{
-		line_size = newline_pos - *stock + 1;
-		line = (char *)malloc(sizeof(char) * line_size + 1);
-		if (!line)
-			return (NULL);
-		ft_strlcpy(line, *stock, line_size + 1);
-		ft_strlcpy(*stock, newline_pos + 1, ft_strlen(newline_pos + 1) + 1);
-	}
-	else
-	{
-		line = ft_strdup(*stock);
-		if (!line)
-			return (free(*stock), NULL);
 		free(*stock);
 		*stock = NULL;
 	}
 	return (line);
 }
 
+char	*ft_finalize_line(char **stock, char *buffer)
+{
+	char	*line;
+
+	if (*stock && **stock)
+	{
+		line = ft_strdup(*stock);
+		if (!line)
+		{
+			free(*stock);
+			*stock = NULL;
+		}
+	}
+	else
+		line = NULL;
+	free(*stock);
+	*stock = NULL;
+	free(buffer);
+	return (line);
+}
+
 char	*get_next_line(int fd)
 {
-	static char	*stock[1024];
+	static char	*stock;
 	char		*buffer;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || fd > 1024)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
-	if (!stock[fd])
+	if (!stock)
 	{
-		stock[fd] = ft_strdup("");
-		if (!stock[fd])
-			return (free(stock[fd]), NULL);
+		stock = ft_strdup("");
+		if (!stock)
+			return (free(buffer), NULL);
 	}
-	stock[fd] = read_and_store(fd, stock[fd], buffer);
-	free(buffer);
-	if (!stock[fd])
-		return (NULL);
-	if (*stock[fd] == '\0')
-		return (free(stock[fd]), stock[fd] = NULL, NULL);
-	line = extract_line(&stock[fd]);
-	return (line);
+	stock = ft_read_and_update_stock(fd, stock, buffer);
+	if (!stock)
+		return (free(buffer), NULL);
+	line = ft_extract_line_from_stock(&stock);
+	if (line)
+		return (free(buffer), line);
+	return (ft_finalize_line(&stock, buffer));
 }
 
 // int	main(int argc, char *argv[])
